d40011261123f7dfe9631a8848b04a9ea417cd22
jenberu <jemberu0970@gmail.com>
2024-06-22 15:11:12 +0300

    coupons/models.py  updated

--- coupons/models.py
+++ coupons/models.py
@@ -6,7 +6,7 @@ class Coupon(models.Model):
     code=models.CharField(max_length=100,unique=True)
     valid_from=models.DateTimeField()
     valid_to=models.DateTimeField()
-    dicount_amount=models.IntegerField(validators=[MinValueValidator(0),MaxValueValidator(100)],help_text='percentage value (0 to 100)')
+    discount_amount=models.IntegerField(validators=[MinValueValidator(0),MaxValueValidator(100)],help_text='percentage value (0 to 100)')
     active=models.BooleanField()
     def __str__(self):
         return self.code
