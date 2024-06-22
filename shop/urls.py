b3773ef1d8e9e278d7bce201cfbaf26aac8bf69f
jenberu <jemberu0970@gmail.com>
2024-06-22 15:09:29 +0300

    shop/urls.py updated

--- shop/urls.py
+++ shop/urls.py
@@ -10,5 +10,7 @@ urlpatterns=[
     path('category/<slug:category_slug>/',views.product_list,name='product_list_by_category'),
     path('<int:id>/<slug:slug>/',views.product_detail,name='product_detail'),
     path('addshop/',views.add_shop,name='add_shop'),
+    path('shopadmin/',views.product_list_for_shop_owner,name='adminhome'),
+
 
 ]
